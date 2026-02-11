8254f9462da927832cce85c3f2730fb046bb5c08
Star-Mdakane <101474151+Star-Mdakane@users.noreply.github.com>
2026-02-11 06:29:17 +0200

    Update home.js clear links

--- starter-code/js/home.js
+++ starter-code/js/home.js
@@ -6,6 +6,11 @@ const menu = document.getElementById("menu");
 const links = document.querySelectorAll("nav a");
 
 links.forEach(link => {
+  link.parentElement.classList.remove("isActive");
+  link.removeAttribute("aria-current");
+});
+
+links.forEach(link => {
     if (link.href.includes(currentPage)) {
         link.parentElement.classList.add("isActive");
         link.setAttribute("aria-current", "page");
